0d0f7d5aacfc3b489caf4c2889708d30dfb2995b
Bruno Gomes <gomes.bruno201@gmail.com>
2023-03-03 23:56:12 -0300

    feat: create a index route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+#[rocket::get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[rocket::launch]
+fn rocket() -> _ {
+    let rocket = rocket::build();
+    let routes = rocket::routes![index];
+
+    rocket.mount("/", routes)
 }
