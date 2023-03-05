22552485a65c7b01e29aa769c2bd25588e16e81c
Bruno Gomes <gomes.bruno201@gmail.com>
2023-03-05 18:51:31 -0300

    refactor: create a cursor class

--- src/templates/script.js
+++ src/templates/script.js
@@ -61,3 +61,103 @@ export class ElementHandler {
     spanList.forEach((span) => this.#appendSpan(span));
   }
 }
+
+export class CursorHandler {
+  constructor(textWrapper) {
+    this.textWrapper = textWrapper;
+
+    this.currentLetterIndex = 0;
+    this.currentWordIndex = 0;
+  }
+
+  #checkDOMState() {
+    const currentWord = this.textWrapper.children[this.currentWordIndex];
+    const currentLetter = currentWord.children[this.currentLetterIndex];
+
+    return {
+      currentWord,
+      currentLetter
+    }
+  }
+
+  #checkState() {
+    const { currentWord, currentLetter } = this.#checkDOMState();
+
+    const isFirstLetter = (this.currentLetterIndex === 0);
+    const isLastLetter = (this.currentLetterIndex === currentWord.children.length - 1);
+
+    const isFirstWord = (this.currentWordIndex === 0);
+    const isLastWord = (this.currentWordIndex === this.textWrapper.children.length - 1);
+
+    const isStartOfText = (isFirstLetter && isFirstWord);
+    const isEndOfText = (isLastLetter && isLastWord);
+
+    return {
+      isFirstLetter,
+      isLastLetter,
+      isFirstWord,
+      isLastWord,
+      isStartOfText,
+      isEndOfText
+    }
+  }
+
+  #goToPreviousLetter() {
+    this.currentLetterIndex -= 1;
+  }
+
+  #goToNextLetter() {
+    this.currentLetterIndex += 1;
+  }
+
+  #goToPreviousWord() {
+    this.currentWordIndex -= 1;
+    this.currentLetterIndex = this.textWrapper.children[this.currentWordIndex].children.length - 1;
+  }
+
+  #goToNextWord() {
+    this.currentWordIndex += 1;
+    this.currentLetterIndex = 0;
+  }
+
+  #activateCursor(state) {
+    const element = this.textWrapper
+      .children[this.currentWordIndex]
+      .children[this.currentLetterIndex]
+
+    const addCursor = () => element.classList.add("cursor");
+    const removeCursor = () => element.classList.remove("cursor");
+
+    state
+      ? addCursor()
+      : removeCursor();
+  }
+
+  previous() {
+    const { isStartOfText, isFirstLetter } = this.#checkState();
+
+    if (isStartOfText) return;
+
+    this.#activateCursor(false);
+
+    isFirstLetter
+      ? this.#goToPreviousWord()
+      : this.#goToPreviousLetter();
+
+    this.#activateCursor(true);
+  }
+
+  next() {
+    const { isEndOfText, isLastLetter } = this.#checkState();
+
+    if (isEndOfText) return;
+
+    this.#activateCursor(false);
+
+    isLastLetter
+      ? this.#goToNextWord()
+      : this.#goToNextLetter();
+
+    this.#activateCursor(true);
+  }
+}
